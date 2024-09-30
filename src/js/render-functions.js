a27c9415819eaeb7dab30dcb8c2aab8017a8f4dc
Nelia Demenska <demenskaya@nelya.gmail.com>
2024-09-30 15:51:56 +0200

    fixed hw

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,16 +1,16 @@
 export function renderImages(images) {
 const gallery = document.getElementById('gallery');
-    const markup = images.map(image => `
-        <a href="${image.largeImageURL}" class="gallery-item">
+const markup = images.map(image => `
+    <a href="${image.largeImageURL}" class="gallery-item">
         <div class="wrapper">
-    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-    </div>
-    <ul class="info">
-        <li>Likes: ${image.likes}</li>
-        <li>Views: ${image.views}</li>
-        <li>Comments: ${image.comments}</li>
-        <li>Downloads: ${image.downloads}</li>
-    </ul>
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </div>
+        <ul class="info">
+            <li>Likes: ${image.likes}</li>
+            <li>Views: ${image.views}</li>
+            <li>Comments: ${image.comments}</li>
+            <li>Downloads: ${image.downloads}</li>
+        </ul>
     </a>
 `).join('');
 gallery.innerHTML = markup;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { fetchImages }  from './js/pixabay-api';
+import { fetchImages }  from './js/pixabay-api'; 
 import { renderImages }  from './js/render-functions';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -7,31 +7,35 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.getElementById('search-form');
 const loader = document.getElementById('loader');
+const galleryElement = document.getElementById('gallery'); 
 let gallery = new SimpleLightbox('.gallery-item');
 
 form.addEventListener('submit', async (e) => {
 e.preventDefault();
 
 const query = e.target.elements.searchQuery.value.trim();
-    if (!query) {
+
+galleryElement.innerHTML = '';
+
+if (!query) {
     iziToast.warning({ message: 'Sorry, there are no images matching your search query. Please try again!' });
     return;
 }
 
-    try {
-    loader.classList.remove('hidden');
-    const data = await fetchImages(query);
-    
-    if (data.hits.length === 0) {
+try {
+loader.classList.remove('hidden');
+const data = await fetchImages(query);
+
+if (data.hits.length === 0) {
     iziToast.error({ message: 'Sorry, there are no images matching your search query. Please try again!' });
-    return;
+        return;
     }
-    
+
     renderImages(data.hits);
     gallery.refresh();
-} catch (error) {
-    iziToast.error({ message: error.message });
-} finally {
-    loader.classList.add('hidden');
-}
+    } catch (error) {
+        iziToast.error({ message: error.message });
+    } finally {
+        loader.classList.add('hidden');
+    }
 });
